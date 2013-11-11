acb565c4439b4895c005007e3f4fb195da01e81a
Oliver Mader <b52@reaktor42.de>
2013-11-11 14:41:32 +0100

    Fix algorithm signature

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-def find_substrings():
-    pass
+def find_substrings(string, min_length=3):
+    return [('asd', [0]), ('bla', [3, 6])]
+
 
 # vim: set expandtab shiftwidth=4 softtabstop=4 textwidth=79:
