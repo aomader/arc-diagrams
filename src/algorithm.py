3a48b39374ce4d3efccee77611ae0e06127519c0
Rafal <rmakaryk@googlemail.com>
2013-11-12 12:04:34 +0100

    implemented find_substring

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 
-def find_substrings(string, min_length=3):
-    return [('asd', [0]), ('bla', [3, 6])]
+from suffix_tree import *
 
+def find_substrings(string, min_length=3):
+    retVal = []
+    gst = GeneralisedSuffixTree([string])
+
+    for shared in gst.sharedSubstrings(min_length):
+        subString = []
+        subIndex = []
+        subString = string[shared[0][1]:shared[0][2]]
+        for seq, start, stop in shared:
+            subIndex.append(start)
+        subIndex.sort()
+        retVal.append((subString, subIndex))
+    return retVal
+
+#find_substrings('assdf43454asdf3465asdasdf324asdfz')
 
 # vim: set expandtab shiftwidth=4 softtabstop=4 textwidth=79:
