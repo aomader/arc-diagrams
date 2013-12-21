de7fcc9d12d164dab337f4fe114aeb00749f07f4
Oliver Mader <b52@reaktor42.de>
2013-12-21 15:16:55 +0100

    Adapt the naive implementation to fulfil the tests

--- arc_diagrams/naive_algorithm.py
+++ arc_diagrams/naive_algorithm.py
@@ -10,12 +10,11 @@ def maximal_matching_pairs(string):
         string (str): The string to be searched.
 
     Returns:
-        list. A list of tuples, each describing one matching pair as composed
-              of the start of the first and the second substring, as well as
-              the length of the substring.
+        generator. A generator of tuples, each describing one matching pair as
+                   composed of the start of the first and the second substring,
+                   as well as the length of the substring.
     """
     n = len(string)
-    pairs = []
 
     for x in range(0, n - 1):
         for l in range(int((n - x)/2) + 1, 0, -1):
@@ -24,17 +23,25 @@ def maximal_matching_pairs(string):
             y = string.find(c, x + l)
 
             # not found or not consecutive
-            if y == -1 or string.find(c, x + 1, y) != -1:
+            if y == -1 or string.find(c, x + 1, y + l - 1) != -1:
                 continue
 
-            # not maximal
-            if any(x1 <= x <= x1 + l1 - l and y1 <= y <= y1 + l1 - l
-                   for x1,y1,l1 in pairs):
+            # not maximal: left or right expansion
+            if (y + l < n and x + l < y and string[x+l] == string[y+l]) or \
+               (x - 1 >= 0 and x + l < y and string[x-1] == string[y-1]):
                 continue
 
-            pairs.append((x, y, l))
+            # not maximal: inner expansion
+            if any(string[x:x+l+i] == string[y-i:y+l]
+                   for i in range(1, (y - x - l)/2 + 1)):
+                continue
 
-    return pairs
+            # not maximal: outer expansion
+            if any(string[x-i:x+l] == string[y:y+l+i]
+                   for i in range(1, max(x, n - y - l) + 1)):
+                continue
+
+            yield (x, y, l)
 
 
 def repetition_regions(string):
